a2ad509485c268846a8ed7c46500f5895e4204d6
EmmA <emma.arlanda@viacesi.fr>
2021-06-13 15:33:25 +0200

    init mobile dev

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  function imageLoaded() {
+    alert('Image chargée');
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Bonjour les DI ! </Text>
+      <Text style={styles.p }>Testons le React-Native</Text>
       <StatusBar style="auto" />
+      <Image style={styles.image}
+      source = {{
+      uri: "https://picsum.photos/536/354"}}
+      accessible={true}
+      accessibilityLabel={'An image from Lorem Picsum'}
+      onLoad={imageLoaded}
+      />
+      <Image style={styles.appicon}
+      source={require('./assets/icon.png')}
+      />
+    <StatusBar style='light'/>
+    
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'black',
+    paddingTop: 50,
+    paddingHorizontal: 20
   },
+  image:{
+    width:300,
+    height:200,
+    marginVertical: 10
+  },
+  appicon:{
+    width:100,
+    height:100,
+    marginVertical: 10
+  },
+
+title:{
+  color:'white',
+  fontSize: 22
+},
+
+  p:{
+    color:'white',
+    fontSize: 16,
+    marginTop: 5
+  }
 });
